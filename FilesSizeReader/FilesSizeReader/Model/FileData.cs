6d9be0f8950e1cfe749a2a1efe85ae5904be09e7
unknown <dima_s500@mail.r>
2019-10-15 23:37:27 +0300

    Залил практически пустой проект на githab

---
+++ FilesSizeReader/FilesSizeReader/Model/FileData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesSizeReader.Model
+{
+	[Serializable]
+	public class FileData
+	{
+		public string Name { get; set; }
+
+
+		[NonSerialized]
+		public string FullName;
+
+		[NonSerialized]
+		public string Extension;
+
+		public long Size { get; set; }
+	}
+}
---
+++ FilesSizeReader/FilesSizeReader/ViewModel/FileViewModel.cs
@@ -0,0 +1,22 @@
+﻿using FilesSizeReader.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesSizeReader.ViewModel
+{
+	public class FileViewModel
+	{
+		private FileData _fileData;
+		
+		public ObservableCollection<FileData> _files { get; set; }
+
+		public void GetFiles(string path) 
+		{ 
+			
+		}
+	}
+}
